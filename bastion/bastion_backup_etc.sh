fb511e595c12bbc21f7dcfae9fb585a61f973662
mrpabon21 <mrpabon21>
2024-03-09 21:56:03 -0500

    fixing backup script to connect by ssh

--- bastion/bastion_backup_etc.sh
+++ bastion/bastion_backup_etc.sh
@@ -7,8 +7,8 @@ filedate=$(date "+%F-%H%M%S")
 echo "### START $(date "+%F-%H%M%S")"
 
 # backup etc configs
-tar -czf /opt/backup/etc_$filedate.tar.gz /etc/
+ssh administrator@192.168.30.3 'tar -czf /tmp/monitor_etc_$filedate.tar.gz /etc/ | scp administrator@192.168.30.3:/tmp/monitor_etc_$filedate.tar.gz /opt/backup/'
 
 echo "### END $(date "+%F-%H%M%S")"
 
-mail -s "Backup finished" user1@server1 < /var/log/backup_files.log
+#mail -s "Backup finished" user1@server1 < /var/log/backup_files.log
