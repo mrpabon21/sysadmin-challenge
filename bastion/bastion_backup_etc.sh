989a89b8e6b06b6f1b085ec089f286bc19924bed
mrpabon21 <mrpabon21>
2024-03-09 22:19:21 -0500

    fixing backup script

--- bastion/bastion_backup_etc.sh
+++ bastion/bastion_backup_etc.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-exec > /var/log/backup_files.log 2>&1
+exec > /var/log/bastion_backup_etc.log | sudo su - 2>&1
 
 filedate=$(date "+%F-%H%M%S")
 
 echo "### START $(date "+%F-%H%M%S")"
 
 # backup etc configs
-ssh administrator@192.168.30.3 'tar -czf /tmp/monitor_etc_$filedate.tar.gz /etc/ | scp administrator@192.168.30.3:/tmp/monitor_etc_$filedate.tar.gz /opt/backup/'
+ssh administrator@192.168.30.3 "sudo tar -czf /tmp/monitor_etc_$filedate.tar.gz /etc/"
+sudo scp administrator@192.168.30.3:/tmp/monitor_etc_$filedate.tar.gz /opt/backup/"
 
 echo "### END $(date "+%F-%H%M%S")"
 
