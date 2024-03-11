d4cc09f485f8632d8c6dd8079ec214b13ef89791
mrpabon21 <mrpabon21>
2024-03-11 18:27:52 -0500

    changes to firewall script

--- bastion_start_firewall.sh
+++ bastion_start_firewall.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # flushing all rules
-iptables -F DOCKER-USER
+iptables -F INPUT
  
 # setting default filter policy
 iptables -P INPUT DROP
@@ -10,17 +10,11 @@ iptables -P INPUT DROP
 iptables -A INPUT -i lo -j ACCEPT
 iptables -A OUTPUT -o lo -j ACCEPT
 
-iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
-iptables -A OUTPUT -m conntrack --ctstate ESTABLISHED -j ACCEPT
-
-# allow icmp incoming traffic
-#iptables -A INPUT -p icmp -j ACCEPT
-
 # open ssh to my host
 iptables -A INPUT -p tcp --dport 22 -s 192.168.30.1 -j ACCEPT
 
 # open grafana port to my host
-#iptables -A INPUT -p tcp --dport 3000 -s 192.168.30.1 -j ACCEPT
+iptables -A INPUT -p tcp --dport 3000 -s 192.168.30.1 -j ACCEPT
 
 # open node_exporter port to my host
-#iptables -A INPUT -p tcp --dport 9100 -j ACCEPT
+iptables -A INPUT -p tcp --dport 9100 -j ACCEPT
