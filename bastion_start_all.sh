0ecbf56b53e278b0ceda69bfa3dbeb4555ef5e0d
mrpabon21 <mrpabon21>
2024-03-10 22:11:29 -0500

    adding host firewall

--- bastion_start_all.sh
+++ bastion_start_all.sh
@@ -7,8 +7,8 @@
 # start_sshd
 ./tmp/bastion_start_sshd.sh &
 
-# apply firewall rules
-./tmp/bastion_firewall.sh &
+# start_firewall
+./tmp/bastion_start_firewall.sh &
 
 # wait for any process to exit
 wait -n
